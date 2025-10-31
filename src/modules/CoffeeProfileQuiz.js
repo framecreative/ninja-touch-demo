337f8c2f72563aa309d981b2ec2332f50ea41968
Karl Langer <karl@framecreative.com.au>
2025-10-31 11:34:13 +1030

    Initial styling of QR code block

--- src/modules/CoffeeProfileQuiz.js
+++ src/modules/CoffeeProfileQuiz.js
@@ -601,7 +601,8 @@ export class CoffeeProfileQuiz {
     populateProfileReveal() {
         document.querySelector('.profile-icon').src = `dist/assets/images/${this.profile.icon}`;
         document.getElementById('profile-title').innerHTML = this.profile.title;
-        // document.getElementById('profile-tagline').textContent = this.profile.tagline;
+        document.getElementById('profile-tagline').textContent = this.profile.tagline;
+        document.querySelector('.profile-qr').src = `dist/assets/images/${this.profile.qr_img}`;
         document.getElementById('profile-description').textContent = this.profile.description1;
 
         // Update profile icon dynamically
